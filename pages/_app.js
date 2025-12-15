6462b34df3d3585fa63d1dbb285fb3661b6563e8
Dindory <22jysong@gmail.com>
2025-12-15 17:37:48 +0900

    Revert "fix: index.html 라우팅 로직 수정"
    This reverts commit d594f932d42f1f78ba7b5da581cf8258c552c37b.

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useRouter } from 'next/router';
 import { ThemeProvider } from '@vapor-ui/core';
 import '@vapor-ui/core/styles.css';
@@ -9,19 +9,6 @@ import { AuthProvider } from '@/contexts/AuthContext';
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
-
-  // CloudFront에서 403/404를 /index.html로 폴백시킨 경우,
-  // 클라이언트 사이드 라우팅이 제대로 작동하도록 보장
-  // 정적 내보내기에서는 동적 라우트([room].js 등)가 클라이언트 사이드에서 처리됨
-  useEffect(() => {
-    if (router.isReady) {
-      // CloudFront 폴백으로 인해 index.html이 로드된 경우,
-      // Next.js 라우터가 자동으로 현재 URL을 처리하도록 보장
-      // 새로고침 시에도 라우터가 올바른 경로를 인식하도록 함
-      // Next.js는 이미 클라이언트 사이드 라우팅을 지원하므로
-      // 라우터가 준비되면 자동으로 현재 URL을 처리함
-    }
-  }, [router.isReady]);
 
   const isErrorPage = router.pathname === '/_error';
   if (isErrorPage) {
